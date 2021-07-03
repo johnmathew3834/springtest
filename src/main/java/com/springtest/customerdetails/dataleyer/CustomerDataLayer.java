a18903ddba3df96855b52ce259a2022711c52527
johnmathew3834 <32087689+johnmathew3834@users.noreply.github.com>
2021-07-03 20:30:25 +0530

    add jenkins

--- src/main/java/com/springtest/customerdetails/dataleyer/CustomerDataLayer.java
+++
@@ -1,9 +0,0 @@
-package com.springtest.customerdetails.dataleyer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springtest.customerdetails.CustomerEntity;
-
-public interface CustomerDataLayer extends JpaRepository<CustomerEntity, Long> {
-
-}
